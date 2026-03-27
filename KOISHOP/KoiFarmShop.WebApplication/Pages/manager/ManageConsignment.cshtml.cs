4ca3522671413d681e63868cbe6e3da80c15c73d
JinJin1503 <Khanhng5776@ut.edu.vn>
2026-03-28 03:56:07 +0700

    KF-24 Lỗi 404 Not Found - Không thể truy cập các trang thuộc cụm manager/Consignment

---
+++ KOISHOP/KoiFarmShop.WebApplication/Pages/manager/ManageConsignment.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using KoiFarmShop.Repositories.Entities;
+using KoiFarmShop.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.WebApplication.Pages.Manager
+{
+    [Authorize(Roles = "Manager, Staff")]
+    public class ManageConsignmentModel : PageModel
+    {
+        private readonly IConsignmentRequestService _consignmentService;
+
+        public ManageConsignmentModel(IConsignmentRequestService consignmentService)
+        {
+            _consignmentService = consignmentService;
+        }
+
+        public List<ConsignmentRequest> Requests { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Lấy toàn bộ danh sách đơn ký gửi (Bán & Chăm sóc)
+            Requests = await _consignmentService.GetAllConsignmentRequestsAsync();
+        }
+
+        // Logic phê duyệt đơn hàng nhanh
+        public async Task<IActionResult> OnPostApproveAsync(int id)
+        {
+            await _consignmentService.ApproveRequestAsync(id); //
+            return RedirectToPage();
+        }
+    }
+}

2bdc215eabc87d48e27663d82db98306ba95f7fa
YenDi05 <yendi1907@gmail.com>
2026-03-27 22:37:33 +0700

    KF-25 [Lỗi bảo mật] Guest và Customer truy cập được giao diện Quản trị (Manager) do thiếu phân quyền Role

---
+++ KOISHOP/KOISHOP/KoiFarmShop.WebApplication/Pages/manager/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using KoiFarmShop.Repositories.Entities;
+using KoiFarmShop.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.WebApplication.Pages.Manager
+{
+    [Authorize(Roles = "Quản lý,Nhân viên bán hàng,giám đốc")]
+    public class IndexModel : PageModel
+	{
+		private readonly IStaffService _staffService;
+
+		// Constructor nhận vào StaffService
+		public IndexModel(IStaffService staffService)
+		{
+			_staffService = staffService;
+		}
+
+		public List<Staff> StaffList { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			// Lấy danh sách nhân viên từ DB
+			StaffList = await _staffService.GetAllStaffAsync();
+		}
+	}
+}
